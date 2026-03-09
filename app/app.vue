2130ae67b8aba6759947dab2aba163e84f08780e
lonewolfyx <877085893@qq.com>
2026-03-09 15:01:46 +0800

    feat(app): replace welcome page with site layout structure

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,15 @@
 <template>
-    <div>
-        <NuxtRouteAnnouncer />
-        <NuxtWelcome />
-    </div>
+    <SiteBody>
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
+    </SiteBody>
 </template>
+
+<script setup lang="ts">
+useHead({
+    htmlAttrs: {
+        class: 'h-full',
+    },
+})
+</script>
---
+++ app/components/SiteBody.vue
@@ -0,0 +1,14 @@
+<template>
+    <Body class="antialiased flex h-full text-base text-background bg-background">
+        <div class="relative h-full w-full bg-[#f8fafc]">
+            <div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(#0000001a_1px,#f8fafc_1px)] bg-[size:16px_16px] " />
+        </div>
+        <slot />
+    </Body>
+</template>
+
+<script lang="ts" setup>
+defineOptions({
+    name: 'SiteBody',
+})
+</script>
