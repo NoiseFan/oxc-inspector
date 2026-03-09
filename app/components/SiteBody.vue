485041b3e6720d7da1b8633ade6f6aa3c9496267
lonewolfyx <877085893@qq.com>
2026-03-09 15:05:35 +0800

    refactor(SiteBody): update component layout structure

--- app/components/SiteBody.vue
+++ app/components/SiteBody.vue
@@ -1,9 +1,13 @@
 <template>
     <Body class="antialiased flex h-full text-base text-background bg-background">
-        <div class="relative h-full w-full bg-[#f8fafc]">
-            <div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(#0000001a_1px,#f8fafc_1px)] bg-[size:16px_16px] " />
+        <div class="absolute inset-0 z-0">
+            <div class="relative h-full w-full bg-[#f8fafc]">
+                <div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(#0000001a_1px,#f8fafc_1px)] bg-[size:16px_16px] " />
+            </div>
         </div>
-        <slot />
+        <div class="flex grow flex-col z-10">
+            <slot />
+        </div>
     </Body>
 </template>
 
