64a5a478bcab01fb559358203de3a1f12a877480
lonewolfyx <877085893@qq.com>
2026-03-09 16:05:48 +0800

    refactor(layout): extract navigation bar component from default layout

---
+++ app/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="flex justify-between items-center p-4 **:data-[slot=separator]:!h-4">
+        <span
+            :class="cn(
+                'text-2xl',
+                'font-mono font-extralight font-stretch-condensed',
+            )"
+        >
+            Oxidation Compiler Config Inspector
+        </span>
+        <div class="flex items-center gap-3">
+            <GitHubLink />
+            <Separator orientation="vertical" />
+            <ModeSwitcher />
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { cn } from '~/lib/utils'
+
+defineOptions({
+    name: 'NavBar',
+})
+</script>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,22 +1,7 @@
 <template>
-    <div class="flex justify-between items-center p-4 **:data-[slot=separator]:!h-4">
-        <span
-            :class="cn(
-                'text-2xl',
-                'font-mono font-extralight font-stretch-condensed',
-            )"
-        >
-            Oxidation Compiler Config Inspector
-        </span>
-        <div class="flex items-center gap-3">
-            <GitHubLink />
-            <Separator orientation="vertical" />
-            <ModeSwitcher />
-        </div>
-    </div>
+    <NavBar />
     <slot />
 </template>
 
 <script lang="ts" setup>
-import { cn } from '~/lib/utils'
 </script>
