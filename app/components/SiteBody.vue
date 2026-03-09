de0a2b174d247a17534f99a3806eaeb8f8b8f229
lonewolfyx <877085893@qq.com>
2026-03-09 15:59:40 +0800

    feat(theme): add dark mode support to site background

--- app/components/SiteBody.vue
+++ app/components/SiteBody.vue
@@ -1,9 +1,14 @@
 <template>
     <Body class="antialiased flex h-full text-base text-foreground bg-zinc-100">
         <div class="absolute inset-0 z-0">
-            <div class="relative h-full w-full bg-[#f8fafc]">
+            <div class="relative h-full w-full bg-[#f8fafc] dark:bg-[#0f172a]">
                 <div
-                    class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(#0000001a_1px,#f8fafc_1px)] bg-[size:16px_16px] "
+                    :class="cn(
+                        'absolute bottom-0 left-0 right-0 top-0',
+                        'bg-[size:16px_16px]',
+                        'bg-[radial-gradient(#0000001a_1px,#f8fafc_1px)]',
+                        'dark:bg-[radial-gradient(#ffffff33_1px,#0f172a_1px)]',
+                    )"
                 />
             </div>
         </div>
@@ -14,6 +19,8 @@
 </template>
 
 <script lang="ts" setup>
+import { cn } from '~/lib/utils'
+
 defineOptions({
     name: 'SiteBody',
 })
