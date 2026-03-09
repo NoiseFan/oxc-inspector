31debe0557b24c666a632d96ca5353f96290085b
lonewolfyx <877085893@qq.com>
2026-03-09 21:41:49 +0800

    chore(config): update head configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,14 +19,9 @@ export default defineNuxtConfig({
         head: {
             viewport: 'width=device-width,initial-scale=1',
             link: [
-                { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
                 { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' },
-                { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
             ],
-            meta: [
-                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-                { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
-            ],
+            title: 'Oxidation Compiler Config Inspector',
         },
     },
 
