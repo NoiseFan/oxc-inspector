39132fa92bc588ac4398722ef2c301657ec4a9fa
lonewolfyx <877085893@qq.com>
2026-03-11 17:55:38 +0800

    feat(navbar): add conditional rendering for navigation menu items

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -31,24 +31,28 @@
             <span class="opacity-50">config items</span>
         </div>
         <div class="flex justify-start items-center gap-4">
-            <Button
+            <template
                 v-for="menu in navMenus"
                 :key="menu.name"
-                :class="cn(
-                    'text-base capitalize',
-                    'rounded shadow-none bg-transparent',
-                    routeName === menu.name ? 'color-active' : 'hover:border-teal-600/25 hover:bg-teal-400/5',
-                )"
-                variant="outline"
             >
-                <NuxtLink :to="`/${menu.name}`" class="flex items-center gap-3">
-                    <Icon
-                        :name="menu.icon"
-                        mode="svg"
-                    />
-                    {{ menu.label }}
-                </NuxtLink>
-            </Button>
+                <Button
+                    v-if="menu.status"
+                    :class="cn(
+                        'text-base capitalize',
+                        'rounded shadow-none bg-transparent',
+                        routeName === menu.name ? 'color-active' : 'hover:border-teal-600/25 hover:bg-teal-400/5',
+                    )"
+                    variant="outline"
+                >
+                    <NuxtLink :to="`/${menu.name}`" class="flex items-center gap-3">
+                        <Icon
+                            :name="menu.icon"
+                            mode="svg"
+                        />
+                        {{ menu.label }}
+                    </NuxtLink>
+                </Button>
+            </template>
         </div>
     </div>
 </template>
@@ -65,43 +69,50 @@ const app = useAppConfig()
 const route = useRoute()
 const routeName = computed(() => route.name as NavBar)
 
+const { metaOptions, oxFormat } = useConfigInspector()
+
 const navMenus = [
     {
         name: 'projects',
         label: 'projects',
         icon: 'ph:diamonds-four-duotone',
+        status: true,
     },
     {
         name: 'configs',
         label: 'configs',
         icon: 'ph:stack-duotone',
+        status: true,
     },
     {
         name: 'rules',
         label: 'rules',
         icon: 'ph:list-dashes-duotone',
+        status: true,
     },
     {
         name: 'oxfmt',
         label: 'oxfmt',
         icon: 'ph:paint-brush-broad-fill',
+        status: oxFormat.value != null,
     },
     {
         name: 'migration-rules',
         label: 'migration rules',
         icon: 'pajamas:merge',
+        status: true,
     },
     {
         name: 'migration-configs',
         label: 'migration configs',
         icon: 'ph:file-code-duotone',
+        status: true,
     },
     {
         name: 'eslint',
         label: 'eslint configs',
         icon: 'ph:stack-minus-duotone',
+        status: true,
     },
 ]
-
-const { metaOptions } = useConfigInspector()
 </script>
