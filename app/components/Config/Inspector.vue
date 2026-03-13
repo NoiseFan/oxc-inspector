36741d7f8d080b30df446c4932b994c33439ebc5
lonewolfyx <877085893@qq.com>
2026-03-13 13:41:40 +0800

    feat(config): add error display and improve loading state handling

--- app/components/Config/Inspector.vue
+++ app/components/Config/Inspector.vue
@@ -1,5 +1,36 @@
 <template>
-    <div v-if="loading" class="flex flex-col w-full h-full justify-center items-center">
+    <div v-if="errorInfo" class="grid h-full w-full place-content-center whitespace-pre-line p-4">
+        <div class="flex flex-col items-center space-y-32">
+            <ConfigInspectorBadge
+                class="mb-6 text-2xl font-extrabold"
+            />
+            <div class="flex flex-col items-center gap-4 text-2xl text-red-500 font-bold">
+                <span>Failed to load</span>
+                <div class="flex items-center gap-2">
+                    <span
+                        v-for="file in lintConfigFilenames"
+                        :key="file"
+                        class="rounded bg-red-500 px-2 text-white text-lg"
+                    >
+                        {{ file }}
+                    </span>
+                </div>
+            </div>
+
+            <div class="text-lg text-red-500 font-mono">
+                {{ errorInfo.message }}
+            </div>
+
+            <div class="mt-6 opacity-50">
+                Note that
+                <NuxtLink :to="`https://github.com/${app.github.repo}`" class="hover:underline" target="_blank">
+                    config inspector
+                </NuxtLink>
+                only works with the oxc config
+            </div>
+        </div>
+    </div>
+    <div v-else-if="loading" class="flex flex-col w-full h-full justify-center items-center">
         <div
             class="flex gap-2 items-center flex-auto animate-pulse text-2xl"
         >
@@ -17,11 +48,13 @@
 </template>
 
 <script lang="ts" setup>
+import { lintConfigFilenames } from '~~/src/constants'
 import { useConfigInspector } from '.'
 
 defineOptions({
     name: 'ConfigInspector',
 })
 
-const { loading } = useConfigInspector() as IConfigInspector
+const app = useAppConfig()
+const { loading, errorInfo } = useConfigInspector() as IConfigInspector
 </script>
